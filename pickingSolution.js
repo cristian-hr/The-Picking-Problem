ef5757285c8017d9a4bfbdad7a15b0ae70a4ddc3
cristian-hr <cristian.hrauh@gmail.com>
2021-06-30 18:42:19 -0400

    feat(pickingSolution) comments added

--- pickingSolution.js
+++ pickingSolution.js
@@ -1,10 +1,14 @@
-
 function articlePicking(scenario) {
     let articles = []
     let count = 0
+    //Recorro un while 4 veces o hasta tener 4 valores
     while (count < 4 && articles.length < 4) {
+        //Recorro propiedad del objeto scenario
         for (let key in scenario) {
-            if (scenario[key][count] && articles.length < 4 ) {
+            //Si ya tengo 4 valores, freno el for.
+            //De lo contrario, si hay data el objeto actual, pusheo su propiedad "token" en articles.
+            if (articles.length === 4) break
+            else if (scenario[key][count]) {
                 articles.push(+scenario[key][count]["token"])
             }
         }
